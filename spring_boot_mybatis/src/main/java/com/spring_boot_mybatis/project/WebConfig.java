216e5538e77bb5b424f0cffb01c71ce4c666f15a
gam02076 <73230338+gam02076@users.noreply.github.com>
2023-02-13 17:25:22 +0900

    Update WebConfig.java

--- spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/WebConfig.java
+++ spring_boot_mybatis/src/main/java/com/spring_boot_mybatis/project/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
-		.addResourceLocations("file:///usr/local/project/product_images/");
+		.addResourceLocations("file:////usr/local/project/product_images/");
 	}
 }
